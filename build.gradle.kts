afcd3fe9cd3ff6473b9f57a8695acc1087beac88
Taylore <tthorntone@gmail.com>
2022-11-24 14:30:08 -0500

    adds to patches

--- build.gradle.kts
+++ build.gradle.kts
@@ -55,12 +55,4 @@ tasks {
         classpath = sourceSets["main"].runtimeClasspath
         mainClass.set("app.revanced.meta.Meta")
     }
-    // Dummy task to fix the Gradle semantic-release plugin.
-    // Remove this if you forked it to support building only.
-    // Tracking issue: https://github.com/KengoTODA/gradle-semantic-release-plugin/issues/435
-    register<DefaultTask>("publish") {
-        group = "publish"
-        description = "Dummy task"
-        dependsOn(named("generateBundle"), named("generateMeta"))
-    }
 }
